cc9cf9408e7fa94a77c23195b90b996752856eed
Asif-newaz <asif.newaz0093@gmail.com>
2020-08-14 02:28:13 +0600

    started dom manipulation

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6 ) +1;
-console.log(dice);
+//console.log(dice);
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
 
 
 
