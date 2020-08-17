e3336c1c00ccb42842da7047d38740c18b206eee
Asif-newaz <asif.newaz0093@gmail.com>
2020-08-18 02:43:35 +0600

    worked js events

--- app.js
+++ app.js
@@ -9,23 +9,37 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6 ) +1;
-//console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6 ) +1;
+    
+    // 2. Display the result
+    var diceDom = document.querySelector('.dice');
+    diceDom.src = 'dice-' + dice + '.png';
+    diceDom.style.display = 'block';
+    
+    
+    // 3. Update the round score IF the rolled number was NOT a 1
+    
+     
+});
 
 
 
@@ -42,3 +56,15 @@ document.querySelector('.dice').style.display = 'none';
 
 
 
+
+
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#score-0').textContent;
