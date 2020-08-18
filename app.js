e55b390786f28baf322dd412d39df77f9bbca5bf
Asif-newaz <asif.newaz0093@gmail.com>
2020-08-19 00:58:11 +0600

    worked with current value

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 
@@ -28,7 +28,7 @@ document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function() {
     
     // 1. Random number
-    var dice = Math.floor(Math.random() * 6 ) +1;
+    var dice = Math.floor(Math.random() * 6 ) + 1;
     
     // 2. Display the result
     var diceDom = document.querySelector('.dice');
@@ -37,7 +37,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     
     
     // 3. Update the round score IF the rolled number was NOT a 1
-    
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;//ternary operator
+        /*
+        if (activePlayer === 0) {
+            activePlayer = 1;
+        } else {
+            activePlayer = 0;
+        }
+        */
+        
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+    }
      
 });
 
