4ce8545d08ded021414a45bee516353212d484d8
Asif-newaz <asif.newaz0093@gmail.com>
2020-08-22 01:38:33 +0600

    hold score implementation done

--- app.js
+++ app.js
@@ -44,27 +44,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;//ternary operator
-        /*
-        if (activePlayer === 0) {
-            activePlayer = 1;
-        } else {
-            activePlayer = 0;
-        }
-        */
-        
-        roundScore = 0;
-        
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        
-//        document.querySelector('.player-0-panel').classList.remove('active');
-//        document.querySelector('.player-1-panel').classList.add('active');
-        
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
         
     }
      
@@ -79,10 +59,47 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
     // Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }
+    else {
+        // Next player
+        nextPlayer();
+    }
+        
 });
 
 
-
+function nextPlayer() {
+    
+    /*
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+    */
+    
+    /* Alternative way to write if else condition */
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;//It's called ternary operator
+        
+    roundScore = 0;
+        
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+        
+//  document.querySelector('.player-0-panel').classList.remove('active');
+//  document.querySelector('.player-1-panel').classList.add('active');
+        
+    document.querySelector('.dice').style.display = 'none';
+    
+}
 
 
 
